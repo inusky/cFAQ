f343e06bf3e99949e452a7385b6739d523e35468
inusky <inuskydotcom@gmail.com>
2026-03-26 19:15:25 +0530

    Add database

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/hints'],
+  modules: ['@nuxt/image', '@nuxt/hints', '@prisma/nuxt'],
   vite: {
     optimizeDeps: {
       include: ['@vue/devtools-core', '@vue/devtools-kit'],
